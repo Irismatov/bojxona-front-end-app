643fb6a092a6789427e88f0b9c24aa1e3674998b
Loki Laufeyson <jakhongirirismatov@gmail.com>
2025-02-28 17:52:29 +0500

    last

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    proxy: {
-      '/api': {                                          // 1
-        target: 'https://e701-84-54-90-60.ngrok-free.app', // 2
-        changeOrigin: true,                              // 3
-        rewrite: (path) => path.replace(/^\/api/, '')    // 4
-      }
-    }
-  }
 })
